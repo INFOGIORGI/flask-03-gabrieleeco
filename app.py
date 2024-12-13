519f223bc648e0b577e8a81e09fb04e858eb5f0e
gabrieleeco <gabrieleconte06@gmail.com>
2024-12-13 12:37:29 +0000

    13-12

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("home.html", titolo="Home")
+
+@app.route("/dettagli")
+def detail():
+    prodotti = (("Laptop", "5", "€599"), ("Tablet", "3", "€450"), ("Smartphone", "2", "€800"), ("TV", "8", "€700"))
+    return render_template("detail.html", titolo="Dettagli", prodotti=prodotti)
+
+@app.route("/scaffale")
+def scaffale():
+    return render_template("dettagliScaffale.html", titolo="Scaffale")
 
 app.run()
