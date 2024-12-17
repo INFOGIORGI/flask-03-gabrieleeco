f2a4b52089ffc68d7ff40253c582267a4cd8b194
gabrieleeco <gabrieleconte06@gmail.com>
2024-12-17 11:15:43 +0000

    17-12

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+prodotti = (("Laptop", "5", "€599"), ("Tablet", "3", "€450"), ("Smartphone", "2", "€800"), ("TV", "8", "€700"))
 
 @app.route("/")
 def home():
@@ -8,11 +9,15 @@ def home():
 
 @app.route("/dettagli")
 def detail():
-    prodotti = (("Laptop", "5", "€599"), ("Tablet", "3", "€450"), ("Smartphone", "2", "€800"), ("TV", "8", "€700"))
     return render_template("detail.html", titolo="Dettagli", prodotti=prodotti)
 
-@app.route("/scaffale")
-def scaffale():
-    return render_template("dettagliScaffale.html", titolo="Scaffale")
+@app.route("/scaffale/<numScaffale>")
+def scaffale(numScaffale):
+    list = []
+    for t in prodotti:
+        if t[1] == numScaffale:
+            list.append(t)
+        
+    return render_template("dettagliScaffale.html", titolo="Scaffale " + numScaffale, numScaffale=numScaffale, prodotti = list)
 
 app.run()
